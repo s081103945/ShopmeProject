6a16b736d499504cdcf40a349f5f556eabb846ee
Aries Lai <arieslch41@gmail.com>
2022-04-10 04:15:24 +0800

    Initial commit

---
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/site/MainController.java
@@ -0,0 +1,12 @@
+package com.shopme.site;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Component
+public class MainController {
+    @GetMapping("")
+    public String viewHomePage(){
+        return "index";
+    }
+}
